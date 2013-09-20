52b44ab7e1c1eda4f74bf80aefd54679061f5c35
Etienne de Bruin <etdebruin@gmail.com>
2013-09-19 18:35:19 -0700

    Init.

---
+++ life.js
@@ -0,0 +1,45 @@
+$(document).ready(function () {
+
+  var life = [];
+
+  life = makeArray(10,10);
+  life = seed(life);
+
+  printArray(life);
+
+
+  function seed(a) {
+
+    for (i = 0; i < a.length; i++) {
+      for (j = 0; j < a[i].length; j++) {
+        a[i][j] = Math.floor((Math.random()*2));
+      }
+    }
+
+    return a;
+  }
+
+  function printArray(a) {
+    var o = "";
+
+    for (i = 0; i < 10; i++) {
+      for (j = 0; j < 10; j++) {
+        o = o + " " + a[i][j];
+      }
+      o = o + "<br />";
+    }
+
+    $('#output').html(o);
+  }
+
+  function makeArray(x, y) {
+    var arr = new Array(x), i;
+    for (i=0; i<y; i++) {
+      arr[i] = new Array(x);
+    }
+    return arr;
+  }
+
+
+
+});
